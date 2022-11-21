0259709a8c649c3e0a01d0471c9584ef24fe71cf
object-Object <object@objectobject.ca>
2022-11-20 19:10:37 -0500

    Use dicts instead of sets of tuples, properly handle unions, and scrape Book type

--- scrape_book_types.py
+++ scrape_book_types.py
@@ -1,47 +1,47 @@
 from collections import defaultdict
-from typing import Any, Mapping
+from typing import Any, Callable, Mapping
 
 from Hexal.doc.collate_data import parse_book as hexal_parse_book
-from hexdecode.hexast import Book
 from HexMod.doc.collate_data import parse_book
 
 if __name__ != "__main__":
     raise Exception("please don't try to actually use this code in production lmao")
 
-
-def update(item: Mapping[str, Any], keys: set[tuple[str, str]], not_required: set[tuple[str, str]]):
-    current_keys = set((k, v.__class__.__name__) for k, v in item.items())
-    not_required |= keys.difference(current_keys)
-    keys |= current_keys
+BookKeys = defaultdict[str, set[str]]
+BookNotRequired = set[str]
 
 
-def unionize(keys: set[tuple[str, str]], not_required: set[tuple[str, str]]) -> set[tuple[str, str]]:
-    new_keys: dict[str, str] = {}
-    for item in keys:
-        k, t = item
-        if k in new_keys:
-            new_keys[k] += f" | {t}"
+def add_keys(item: Mapping[str, Any], keys: BookKeys, _: BookNotRequired) -> None:
+    for key, value in item.items():
+        keys[key].add(value.__class__.__name__)
+
+
+def add_not_required(item: Mapping[str, Any], keys: BookKeys, not_required: BookNotRequired) -> None:
+    not_required |= set(keys.keys()).difference(set(item.keys()))
+
+
+def set_value_where(keys: BookKeys, key: str, value: set[str], new_value: set[str]) -> None:
+    if key in keys:
+        if keys[key] == value:
+            keys[key] = new_value
         else:
-            new_keys[k] = t
-        if item in not_required:
-            not_required.remove(item)
-            not_required.add((k, new_keys[k]))
-    return set(new_keys.items())
+            raise ValueError(key, value, keys[key])
+    else:
+        raise KeyError(key)
 
 
-def print_class(
-    name: str, keys: set[tuple[str, str]], not_required: set[tuple[str, str]], total=True, parent="TypedDict"
-):
+def print_class(name: str, keys: BookKeys, not_required: BookNotRequired, total=True, parent="TypedDict"):
     print(f"\nclass {name}({parent}{', total=False' if not total else ''}):")
-    for item in sorted(unionize(keys, not_required), key=lambda i: (i in not_required, i[0])):
-        key, value = item
+    for item in sorted(keys.items(), key=lambda i: (i[0] in not_required, i[0])):
+        key, values = item
+        value = " | ".join(values)
         if total:
-            if item in not_required:
+            if key in not_required:
                 print(f"    {key}: NotRequired[{value}]")
             else:
                 print(f"    {key}: {value}")
         else:
-            if item in not_required:
+            if key in not_required:
                 print(f"    {key}: {value}")
             else:
                 print(f"    {key}: Required[{value}]")
@@ -49,24 +49,23 @@ def print_class(
         print("    pass")
 
 
-hex_book: Book = parse_book("HexMod/Common/src/main/resources", "hexcasting", "thehexbook")
-hexal_book: Book = hexal_parse_book(
+hex_book: dict = parse_book("HexMod/Common/src/main/resources", "hexcasting", "thehexbook")
+hexal_book: dict = hexal_parse_book(
     "Hexal/Common/src/main/resources", "Hexal/doc/HexCastingResources", "hexal", "hexalbook"
 )
 
-category_keys: set[tuple[str, str]] = set()
-category_not_required: set[tuple[str, str]] = set()
-entry_keys: set[tuple[str, str]] = set()
-entry_not_required: set[tuple[str, str]] = set()
-page_types: defaultdict[
-    str,
-    tuple[
-        set[tuple[str, str]],
-        set[tuple[str, str]],
-    ],
-] = defaultdict(lambda: (set(), set()))
+book_keys: BookKeys = defaultdict(set)
+book_not_required: BookNotRequired = set()
+category_keys: BookKeys = defaultdict(set)
+category_not_required: BookNotRequired = set()
+entry_keys: BookKeys = defaultdict(set)
+entry_not_required: BookNotRequired = set()
+page_types: defaultdict[str, tuple[BookKeys, BookNotRequired]] = defaultdict(lambda: (defaultdict(set), set()))
 
-for _ in range(2):  # this needs to happen twice for, uh, algorithm reasons
+
+def update_book(update: Callable[[Mapping[str, Any], BookKeys, BookNotRequired], None]) -> None:
+    update(hex_book, book_keys, book_not_required)
+    update(hexal_book, book_keys, book_not_required)
     for category in hex_book["categories"] + hexal_book["categories"]:
         update(category, category_keys, category_not_required)
         for entry in category["entries"]:
@@ -74,20 +73,35 @@ for _ in range(2):  # this needs to happen twice for, uh, algorithm reasons
             for page in entry["pages"]:
                 page_keys, page_not_required = page_types[page["type"]]
                 update(page, page_keys, page_not_required)
-                if page["type"] == "hexcasting:manual_pattern" and "anchor" in page:
-                    print(page)
+                # if update == add_keys and page["type"] == "hexcasting:manual_pattern" and "op_id" not in page:
+                #     print(page, end="\n\n")
+
+
+update_book(add_keys)
+update_book(add_not_required)
+
+set_value_where(entry_keys, "pages", {"list"}, {f"list[BookPage]"})
+set_value_where(category_keys, "entries", {"list"}, {f"list[BookEntry]"})
+set_value_where(book_keys, "categories", {"list"}, {f"list[BookCategory]"})
 
 print(
-    """T = TypeVar("T", bound=LiteralString)
+    """from typing import Generic, Literal, LiteralString, NotRequired, TypedDict, TypeVar
+from HexMod.doc.collate_data import FormatTree
+
+T = TypeVar("T", bound=LiteralString)
 class BookPage(TypedDict, Generic[T]):
     type: T"""
 )
+
 for t, (page_keys, page_not_required) in page_types.items():
+    page_keys.pop("type")
     print_class(
         f"BookPage_{t.replace(':', '_')}",
-        page_keys.difference([("type", "str")]),
+        page_keys,
         page_not_required,
         parent=f'BookPage[Literal["{t}"]]',
     )
+
 print_class("BookEntry", entry_keys, entry_not_required)
 print_class("BookCategory", category_keys, category_not_required)
+print_class("Book", book_keys, book_not_required)
