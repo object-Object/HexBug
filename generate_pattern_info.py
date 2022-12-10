8a3b20ac538944e39d11f55185a97173c0477672
object-Object <object@objectobject.ca>
2022-12-10 03:57:01 -0500

    New extension pattern info gen

---
+++ generate_pattern_info.py
@@ -0,0 +1,54 @@
+# used for my vscode extension
+import json
+from typing import TypedDict
+
+from hexdecode.buildpatterns import build_registry
+from utils.generate_image import Palette, generate_image
+from utils.mods import ModName
+from utils.urls import build_book_url
+
+
+class PatternInfo(TypedDict):
+    name: str
+    modName: ModName
+    filename: str | None
+    direction: str | None
+    pattern: str | None
+    args: str | None
+    url: str | None
+
+
+registry = build_registry()
+output: dict[str, PatternInfo] = {}  # translation: PatternJSON
+for name, translation in registry.name_to_translation.items():
+    filename = name.replace("/", "_") + ".png" if name not in ["mask", "number"] else None
+
+    direction, pattern, is_great = None, None, None
+    if values := registry.translation_to_pattern.get(translation):
+        direction, pattern, is_great, _ = values
+
+        if filename is not None:
+            image = generate_image(direction, pattern, is_great, Palette.Classic, 10, 2)
+            with open(f"out/patterns/{filename}", "wb") as f:
+                f.write(image.getbuffer())
+
+    mod, url = registry.name_to_url.get(name, (None, None))
+    if mod is None:  # invalid pattern
+        continue
+
+    if args := registry.name_to_args.get(name):
+        args = args.replace("**", "").replace("__", "")
+
+    output[translation] = {
+        "name": name,
+        "modName": mod,
+        "filename": filename,
+        "direction": direction.name if direction is not None else None,
+        "pattern": pattern,
+        "args": args,
+        "url": build_book_url(mod, url, False, False) if url is not None else None,
+    }
+
+
+with open("out/registry.json", "w", encoding="utf-8") as f:
+    json.dump(output, f)
--- generate_pattern_json.py
+++
@@ -1,8 +0,0 @@
-import json
-
-from hexdecode.buildpatterns import build_registry
-
-# used for my vscode extension
-registry = build_registry()
-with open("registry.json", "w", encoding="utf-8") as f:
-    json.dump(registry.name_to_translation, f)
