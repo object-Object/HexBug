98d042e95be824d9993c7ae24c06efc7298e5504
object-Object <object@objectobject.ca>
2024-01-02 18:41:21 -0500

    Fix pip error in AfterInstall

--- scripts/aws/after-install.sh
+++ scripts/aws/after-install.sh
@@ -4,8 +4,7 @@ set -euo pipefail
 cd /var/lib/codedeploy-apps/HexBug
 
 python3.11 -m venv venv #--clear
-source venv/bin/activate
 # TODO: hack
 # we need the rust toolchain to build hexnumgen, and i don't want to install it just for codedeploy
 # so run as my user where rust is already installed
-sudo su object -c 'pip install -e ".[runtime]"'
+sudo su object -c 'source venv/bin/activate && pip install -e ".[runtime]"'
