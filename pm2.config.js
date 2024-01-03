c27613bcf9172c4ac2dce51d8487bc498e71b5fc
object-Object <object@objectobject.ca>
2024-01-02 21:47:27 -0500

    Re-tweak health timings

--- pm2.config.js
+++ pm2.config.js
@@ -2,7 +2,7 @@ module.exports = {
     apps: [{
         name: "HexBug",
         script: "./scripts/pm2/run.sh",
-        min_uptime: "10s",
+        min_uptime: "15s",
         max_restarts: 3,
     }]
 }
--- scripts/aws/validate-service.sh
+++ scripts/aws/validate-service.sh
@@ -18,22 +18,19 @@ attempts=3
 want_uuid="$(uuidgen)"
 
 for (( i=1; i<=attempts; i++ )); do
-    # send webhook message
+    echo "Waiting for startup. ($i/$attempts)"
+    sleep 10s
+
     echo "Sending message. ($i/$attempts)"
     curl -H "Content-Type: application/json" -d "{\"content\":\"<@$bot_id> health_check $want_uuid\"}" "$webhook"
     sleep 2s
 
-    # check the response
+    echo "Checking response. ($i/$attempts)"
     if got_uuid=$(<health_check.txt); then
         if [[ "$want_uuid" == "$got_uuid" ]]; then
             echo "Got expected UUID, health OK."
             exit 0
         fi
-    fi
-
-    # give it some time before the next attempt
-    if [[ $i -lt $attempts ]]; then
-        sleep 10s
     fi
 done
 
