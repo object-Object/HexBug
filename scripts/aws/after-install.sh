6ba741a74cafc4a779024d46ef32cca5436ec797
object-Object <object@objectobject.ca>
2024-01-02 19:03:00 -0500

    Vendor wheel for hexnumgen so we don't have to build it

--- scripts/aws/after-install.sh
+++ scripts/aws/after-install.sh
@@ -4,7 +4,5 @@ set -euox pipefail
 cd /var/lib/codedeploy-apps/HexBug
 
 python3.11 -m venv venv #--clear
-# TODO: hack
-# we need the rust toolchain to build hexnumgen, and i don't want to install it just for codedeploy
-# so run as my user where rust is already installed
-sudo su object -c 'source venv/bin/activate && pip install -e ".[runtime]"'
+source venv/bin/activate
+pip install -e ".[runtime]"
