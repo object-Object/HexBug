eddb31b0049b38d9d9bab7b04c147f3a4d9cd0a6
object-Object <object@objectobject.ca>
2024-03-16 00:25:51 -0400

    Add /decode file command for decoding larger pattern lists

--- src/HexBug/cogs/decode.py
+++ src/HexBug/cogs/decode.py
@@ -12,7 +12,7 @@ from ..utils.buttons import build_show_or_delete_button
 from ..utils.commands import HexBugBot
 
 
-class DecodeCog(commands.Cog):
+class DecodeCog(commands.GroupCog, name="decode"):
     def __init__(self, bot: HexBugBot) -> None:
         self.bot = bot
         self.registry = bot.registry
@@ -24,7 +24,7 @@ class DecodeCog(commands.Cog):
         tab_width="The amount of spaces per indentation level",
         show_to_everyone="Whether the result should be visible to everyone, or just you (to avoid spamming)",
     )
-    async def decode(
+    async def text(
         self,
         interaction: discord.Interaction,
         data: str,
@@ -32,6 +32,51 @@ class DecodeCog(commands.Cog):
         show_to_everyone: bool = False,
     ):
         """Decode a pattern list using hexdecode"""
+        await self._decode(interaction, data, tab_width, show_to_everyone)
+
+    @app_commands.command()
+    @app_commands.describe(
+        file="The result of calling Reveal on your pattern list",
+        tab_width="The amount of spaces per indentation level",
+        show_to_everyone="Whether the result should be visible to everyone, or just you (to avoid spamming)",
+    )
+    async def file(
+        self,
+        interaction: discord.Interaction,
+        file: discord.Attachment,
+        tab_width: app_commands.Range[int, 1, 16] = 4,
+        show_to_everyone: bool = False,
+    ):
+        """Decode the contents of a file using hexdecode"""
+        if not file.filename.endswith(".txt"):
+            return await interaction.followup.send(
+                "❌ Only `.txt` files are supported.",
+                ephemeral=True,
+            )
+
+        if file.size > 25_000:
+            return await interaction.followup.send(
+                "❌ File is too large.",
+                ephemeral=True,
+            )
+
+        try:
+            data = (await file.read()).decode()
+        except UnicodeDecodeError:
+            return await interaction.followup.send(
+                "❌ Failed to decode file. Only plain text is supported.",
+                ephemeral=True,
+            )
+
+        await self._decode(interaction, data, tab_width, show_to_everyone)
+
+    async def _decode(
+        self,
+        interaction: discord.Interaction,
+        data: str,
+        tab_width: int,
+        show_to_everyone: bool,
+    ):
         await interaction.response.defer(ephemeral=not show_to_everyone, thinking=True)
 
         try:
