32084a12db02ddac95eb1079454fc68241bc6bd8
object-Object <object@objectobject.ca>
2024-05-07 14:22:33 -0400

    Tweak health check timeouts

--- codedeploy/hooks/validate-service.sh
+++ codedeploy/hooks/validate-service.sh
@@ -6,7 +6,7 @@ cd /var/lib/codedeploy-apps/HexBug
 docker compose exec \
     --env HEALTH_CHECK_DISPLAY_NAME=CodeDeploy \
     --env HEALTH_CHECK_PORT=40405 \
-    --env HEALTH_CHECK_STARTUP_DELAY=45 \
+    --env HEALTH_CHECK_STARTUP_DELAY=60 \
     --env HEALTH_CHECK_ATTEMPTS=3 \
     --env HEALTH_CHECK_INTERVAL=30 \
     bot \
