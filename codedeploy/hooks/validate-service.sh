4297951ce40b37e75f31bc9f6fa070f6499cef4e
object-Object <object@objectobject.ca>
2024-09-28 19:41:40 -0400

    Print docker logs if validate service fails

--- codedeploy/hooks/validate-service.sh
+++ codedeploy/hooks/validate-service.sh
@@ -3,11 +3,15 @@ set -euox pipefail
 
 cd /var/lib/codedeploy-apps/HexBug
 
-docker compose exec \
+if ! docker compose exec \
     --env HEALTH_CHECK_DISPLAY_NAME=CodeDeploy \
     --env HEALTH_CHECK_PORT=40405 \
     --env HEALTH_CHECK_STARTUP_DELAY=60 \
     --env HEALTH_CHECK_ATTEMPTS=3 \
     --env HEALTH_CHECK_INTERVAL=30 \
     bot \
-    python scripts/bot/health_check.py
+    python scripts/bot/health_check.py \
+; then
+    docker compose logs
+    exit 1
+fi
