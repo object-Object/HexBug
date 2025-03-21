c8da2092460c668959f38a491a5c1a9d45003fcb
object-Object <object@objectobject.ca>
2025-03-21 14:52:34 -0400

    Add isPerWorld to vscode extension registry and remove pregenerated pattern images

--- scripts/dev/generate_pattern_info.py
+++ scripts/dev/generate_pattern_info.py
@@ -2,7 +2,6 @@
 import asyncio
 import json
 import logging
-import shutil
 import sys
 from pathlib import Path
 from typing import TypedDict
@@ -10,23 +9,16 @@ from typing import TypedDict
 from aiohttp import ClientSession
 from HexBug.hexdecode.buildpatterns import build_registry
 from HexBug.hexdecode.registry import SpecialHandlerPatternInfo
-from HexBug.rendering import Palette, Theme, draw_patterns, get_grid_options
 from tqdm import tqdm
-
-
-class ImageInfo(TypedDict):
-    filename: str
-    height: int
-    width: int
 
 
 class ExtensionPatternInfo(TypedDict):
     name: str
     modid: str
     modName: str
-    image: ImageInfo | None
     direction: str | None
     pattern: str | None
+    isPerWorld: bool
     args: str | None
     url: str | None
     description: str | None
@@ -44,34 +36,17 @@ if (registry := asyncio.run(_build_registry())) is None:
     sys.exit(1)
 
 out_dir = Path("out")
-patterns_dir = out_dir / "patterns"
-
-shutil.rmtree(patterns_dir, ignore_errors=True)
 
 out_dir.mkdir(parents=True, exist_ok=True)
-patterns_dir.mkdir(parents=True, exist_ok=True)
-for theme in Theme:
-    (patterns_dir / theme.name.lower()).mkdir(parents=True, exist_ok=True)
 
 output: dict[str, ExtensionPatternInfo] = {}  # translation: PatternJSON
 for info in tqdm(registry.patterns):
     if info.translation is None:
         continue
 
-    filename, width, height, direction = None, None, None, None
-
+    direction = None
     if not isinstance(info, SpecialHandlerPatternInfo):
-        filename = info.name.replace("/", "_") + ".png"
         direction = info.direction.name
-        for theme in Theme:
-            options = get_grid_options(
-                palette=Palette.Classic,
-                theme=theme,
-                per_world=info.is_great,
-            )
-            image = draw_patterns((info.direction, info.pattern), options)
-            width, height = image.size
-            image.save(patterns_dir / theme.name.lower() / filename)
 
     args = info.args and info.args.replace("**", "").replace("__", "")
 
@@ -79,15 +54,9 @@ for info in tqdm(registry.patterns):
         "name": info.name,
         "modid": info.mod.value.modid,
         "modName": info.mod.value.name,
-        "image": {
-            "filename": filename,
-            "height": height,
-            "width": width,
-        }
-        if filename and height and width
-        else None,
         "direction": direction,
         "pattern": info.pattern,
+        "isPerWorld": info.is_great,
         "args": args,
         "url": info.mod.value.build_book_url(info.book_url, False, False)
         if info.book_url is not None
