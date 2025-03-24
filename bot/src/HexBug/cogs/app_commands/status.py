cf6c86ff738227756a0572c17a3df90411aacf54
object-Object <object@objectobject.ca>
2025-03-24 15:46:02 -0400

    Implement /status

---
+++ bot/src/HexBug/cogs/app_commands/status.py
@@ -0,0 +1,99 @@
+import textwrap
+from datetime import datetime
+
+from discord import Color, Embed, Interaction, app_commands
+from more_itertools import ilen
+
+from HexBug.common.__version__ import VERSION
+from HexBug.core.cog import HexBugCog
+from HexBug.core.translator import translate_text
+from HexBug.utils.discord.visibility import MessageVisibility, respond_with_visibility
+
+
+class StatusCog(HexBugCog):
+    @app_commands.command()
+    async def status(
+        self,
+        interaction: Interaction,
+        visibility: MessageVisibility = "private",
+    ):
+        if info := self.env.deployment:
+            color = Color.green()
+            commit_url = (
+                f"https://github.com/object-Object/HexBug/commit/{info.commit_sha}"
+            )
+            commit_info = textwrap.dedent(
+                f"""\
+                    [{info.short_commit_sha}]({commit_url}): {info.commit_message}
+                    {_discord_date(info.commit_timestamp)}"""
+            )
+            deployment_time_info = _discord_date(info.timestamp)
+        else:
+            color = Color.orange()
+            commit_info = await translate_text(interaction, "commit_unknown")
+            deployment_time_info = await translate_text(
+                interaction, "deployment-time_unknown"
+            )
+
+        app_info = await self.bot.application_info()
+
+        embed = (
+            Embed(
+                title=await translate_text(interaction, "title"),
+                color=color,
+            )
+            .set_footer(text=f"v{VERSION}")
+            .add_field(
+                name=await translate_text(interaction, "commit"),
+                value=commit_info,
+                inline=False,
+            )
+            .add_field(
+                name=await translate_text(interaction, "deployment-time"),
+                value=deployment_time_info,
+                inline=False,
+            )
+            .add_field(
+                name=await translate_text(interaction, "uptime"),
+                value=_discord_date(self.bot.start_time),
+                inline=False,
+            )
+            .add_field(
+                name=await translate_text(interaction, "installs"),
+                value=await translate_text(
+                    interaction,
+                    "installs_value",
+                    servers=app_info.approximate_guild_count,
+                    users=app_info.approximate_user_install_count,
+                ),
+            )
+            .add_field(
+                name=await translate_text(interaction, "commands"),
+                value=f"{ilen(self.bot.tree.walk_commands())}",
+            )
+            .add_field(
+                name=await translate_text(interaction, "mods"),
+                value=f"{len(self.bot.registry.mods)}",
+            )
+            .add_field(
+                name=await translate_text(interaction, "patterns"),
+                value=f"{len(self.bot.registry.patterns)}",
+            )
+            .add_field(
+                name=await translate_text(interaction, "special-handlers"),
+                value=f"{len(self.bot.registry.special_handlers)}",
+            )
+        )
+
+        await respond_with_visibility(interaction, visibility, embed=embed)
+
+
+def _discord_date(timestamp: int | float | datetime):
+    match timestamp:
+        case int():
+            pass
+        case float():
+            timestamp = int(timestamp)
+        case datetime():
+            timestamp = int(timestamp.timestamp())
+    return f"<t:{timestamp}:f> (<t:{timestamp}:R>)"
--- bot/src/HexBug/resources/lang/en-US/main.ftl
+++ bot/src/HexBug/resources/lang/en-US/main.ftl
@@ -17,3 +17,54 @@ pattern-check_text_title =
         [one] Conflict found!
         *[other] Conflicts found!
     }
+
+# /status
+
+status_description =
+    Show information about HexBug.
+
+status_parameter-description_visibility =
+    {-parameter-description_visibility}
+
+status_text_title =
+    HexBug Status
+
+status_text_commit =
+    Deployed commit
+
+status_text_commit_unknown =
+    Unknown
+
+status_text_deployment-time =
+    Deployment time
+
+status_text_deployment-time_unknown =
+    Unknown
+
+status_text_uptime =
+    Uptime
+
+status_text_installs =
+    Install count
+
+status_text_installs_value =
+    { $servers ->
+        [one] 1 server
+        *[other] { $servers } servers
+    }
+    { $users ->
+        [one] 1 individual user
+        *[other] { $users } individual users
+    }
+
+status_text_commands =
+    Commands
+
+status_text_mods =
+    Mods
+
+status_text_patterns =
+    Patterns
+
+status_text_special-handlers =
+    Special handlers
