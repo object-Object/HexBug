717e3ef796af8ad1434b55d5f7f09f29421dc757
PoolloverNathan <nathan.kulzer@protonmail.com>
2025-11-06 12:32:06 -0500

    disambiguate Transformation Purif

--- data/src/HexBug/data/static_data.py
+++ data/src/HexBug/data/static_data.py
@@ -545,7 +545,10 @@ PATTERN_NAME_OVERRIDES: dict[ResourceLocation, str] = {
 }
 
 # append the mod's name to the pattern's name
-DISAMBIGUATED_PATTERNS: set[ResourceLocation] = set()
+DISAMBIGUATED_PATTERNS: set[ResourceLocation] = {
+    ResourceLocation("moreiotas", "matrix/make"),
+    ResourceLocation("hexstruction", "transformations"),
+}
 
 DISABLED_PAGES: set[str] = set()
 
